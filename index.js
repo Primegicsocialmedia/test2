e5832b9d6caddf4862de9b15d098be8ad1aefe69
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-08-08 18:33:28 +0200

    slideshows

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
+    // Increment slide index
+    slideIndex++;
+    
+    // If slideIndex exceeds the number of slides, reset to 1
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    // Display the current slide
+    slides[slideIndex - 1].style.display = "block";
+
+    // Set a timeout to show the next slide after 3 seconds
+    setTimeout(showSlides, 2000);
 }
